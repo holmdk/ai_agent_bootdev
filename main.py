f955aa5f8365ab9818e7093cc09b3be65dad7f3f
Andreas Holm <andreashn@gmail.com>
2025-06-14 19:44:12 +0200

    New commit

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 
 from google import genai
+from google.genai import types
 
 client = genai.Client(api_key=api_key)
 
@@ -17,8 +18,16 @@ if __name__ == '__main__':
     except:
         sys.exit("1")
 
-    response = client.models.generate_content(model="gemini-2.0-flash-001",
-                                          contents=sys.argv[1])
+    #response = client.models.generate_content(model="gemini-2.0-flash-001",
+    #                                      contents=sys.argv[1])
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+    ]
+
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
 
     print(response.text)
 
